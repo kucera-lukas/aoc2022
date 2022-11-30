ac694bf2d31a558dc6ff4d9d3666b12a11625f91
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-11-30 20:09:29 +0100

    fix reference to abbreviation over function

--- .activate.sh
+++ .activate.sh
@@ -1,2 +1,2 @@
-# uses my fish shell function (see my dotfiles for source https://github.com/kucera-lukas/dotfiles)
+# uses my fish shell abbreviation (see my dotfiles for source https://github.com/kucera-lukas/dotfiles)
 activate
